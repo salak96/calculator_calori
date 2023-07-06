7444c0dec943208d5334e4ab3abe3991e7a8ec3e
lambang <sasangkakurniawan.com>
2023-07-06 11:17:06 +0700

    header

---
+++ calculator_calori-project/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Header from './assets/components/Header';
+
+function App() {
+    return (
+        <>
+            <Header title={'Calorie Calculator'}/>
+        </>
+    );
+}
+
+export default App;
---
+++ calculator_calori-project/src/assets/components/Header.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+import React from "react";
+
+export default function Header ({title}) {
+    return (
+        <div className="container">
+            <h1>{title}</h1>
+        </div>
+    );
+}
